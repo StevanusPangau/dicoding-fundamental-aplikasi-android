b7b50a90a9f8447d4585ef994de4ccc14791d9df
Evan <stevanuspangau50@gmail.com>
2024-03-12 22:34:44 +0700

    upload MyBottomNavigation

---
+++ DicodingMyTabLayout/app/src/main/java/com/learn/dicodingmytablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.learn.dicodingmytablayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        // return 2
+        return 3
+    }
+
+    // contoh membuat tab layout dengan dua fragment
+//    override fun createFragment(position: Int): Fragment {
+//        var fragment: Fragment? = null
+//        when (position) {
+//            0 -> fragment = HomeFragment()
+//            1 -> fragment = ProfileFragment()
+//        }
+//        return fragment as Fragment
+//    }
+
+    // contoh membuat tab layout hanya dengan satu fragment tapi beda data
+    override fun createFragment(position: Int): Fragment {
+        val fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+        }
+        return fragment
+    }
+
+}
